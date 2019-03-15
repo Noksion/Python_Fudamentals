ddad43e335f1c81433100c2b3cb7b3bb54b67082
Ruslan <olkhovskyrus@gmail.com>
2019-03-15 12:46:09 +0200

    Added input

--- alternative.py
+++ alternative.py
@@ -1,4 +1,8 @@
-for i in range(1, 10):
+name1 = int(input("Please enter lower: "))
+name2 = int(input("Please enter upper: "))
+
+
+for i in range(name1, name2):
     x = 2
     c = True
     while i > x:
@@ -9,6 +13,10 @@ for i in range(1, 10):
             x = x + 1
     if c is True:
         print(str(i) + ' is a prime number')
+    else:
+        pass
+
+    if c is True:
         print(str(i) + ' является простым числом')
     else:
         pass
---
+++ main.py
@@ -0,0 +1,2 @@
+test_list = [1, 2, 3, 4, 5]
+print(test_list[1])
