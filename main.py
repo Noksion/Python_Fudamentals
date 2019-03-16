1e47f16c37c299ec46327ed13e4548d17bc0abeb
Ruslan <olkhovskyrus@gmail.com>
2019-03-16 16:00:53 +0200

    Updated

--- main.py
+++ main.py
@@ -1,19 +1,2 @@
-# Игра в угадывание чисел.
-# Когда освою генератор рандомных чисел - значение переменной check будет генерироваться им
-# И можно будет полноценно играть.
-import random
-
-check = random.randint(1, 30)
-
-while True:
-    digit = int(input('Try to guess the number: '))
-
-    if digit < check:
-        print('Too low, try again!')
-
-    if digit > check:
-        print('Too high, try again!')
-
-    if digit == check:
-        print("That's correct! Have a nice time!")
-        break
+a = 3/2
+print(a)
