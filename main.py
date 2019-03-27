2690f67d1adacbc180c2221162c35569217ec76c
Ruslan <olkhovskyrus@gmail.com>
2019-03-27 14:40:50 +0200

    Functions improved a bit

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+name = input("Hello. What's your name? : ")
+
+
 def bmi():
     """Calculates user's Body Mass Index"""
     m = float(input('What is your mass (kilograms)? : '))
     h = float(input('What is you height? (meters) : '))
     result = m / (h**2)
-    print("Your Body Mass Index is: " + str(result))
+    print(name + ", your Body Mass Index is: " + str(result))
 
 
 def greeting():
-    """Getting the user's name and greeting them"""
-    name = input("Hello. What's your name? : ")
-    print("Ok " + name.title() + " Let's get to BMI test.")
+    """Greeting the user"""
+    print("Ok " + name.title() + ", Let's get to BMI test.")
 
 
 greeting()
