7b92fa046e5480b7ff647dafeacf8cf60137809d
Ruslan <olkhovskyrus@gmail.com>
2019-03-27 14:25:32 +0200

    simple functions

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-def pure_pop(list):
-    return list[0: -1]
+def bmi():
+    """Calculates user's Body Mass Index"""
+    m = float(input('What is your mass (kilograms)? : '))
+    h = float(input('What is you height? (meters) : '))
+    result = m / (h**2)
+    print("Your Body Mass Index is: " + str(result))
 
-m = 45.3
-h = 1.59
-imt = m / (h**2)
-print(imt)
 
+def greeting():
+    """Getting the user's name and greeting them"""
+    name = input("Hello. What's your name? : ")
+    print("Ok " + name.title() + " Let's get to BMI test.")
+
+
+greeting()
+bmi()
