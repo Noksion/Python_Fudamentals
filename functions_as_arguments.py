7765b64e99cd1a0bd578101839e1791005878f13
Serhii Kirichko <setti@setti.biz>
2019-03-28 13:08:14 +0200

    Functional programming in Python

--- functions_as_arguments.py
+++ functions_as_arguments.py
@@ -1,14 +1,3 @@
-def multiplication(a, b):
-    return a * b
+from functools import partial
 
-
-def curing(fn, a):
-    def new_function(x):
-        return fn(x, a)
-
-    return new_function
-
-
-multiply_by_two = curing(multiplication, 2)
-
-print(multiply_by_two(10))
+print(partial(lambda x, y: x * y, 2)(10))
