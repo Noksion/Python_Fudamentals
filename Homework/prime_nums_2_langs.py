b2ccb62fdf3deb07061749b8266fc25b53133bc2
Ruslan <olkhovskyrus@gmail.com>
2019-03-30 14:32:37 +0200

    Made prime number generator print separate messages in ENglish and Russian

---
+++ Homework/prime_nums_2_langs.py
@@ -0,0 +1,33 @@
+# Выводит два отдельных списка, Рус и Англ.
+
+num1 = int(input("Please enter lowest number: "))
+num2 = int(input("Please enter highest number: "))
+
+
+# noinspection PyShadowingNames
+def prime_gen(name1, name2):
+    prime_list = []
+    for k in range(name1, 1 + name2):
+        x = 2
+        c = True
+        while k > x:
+            if k % x == 0:
+                c = False
+                x = x + 1
+            else:
+                x = x + 1
+        if c is True:
+            prime_list.append(k)
+        else:
+            pass
+    return prime_list
+
+
+material = prime_gen(num1, num2)
+
+print('')
+for i in material:
+    print(str(i) + ' is a prime number')
+print('')
+for i in material:
+    print(str(i) + ' Является простым числом')
