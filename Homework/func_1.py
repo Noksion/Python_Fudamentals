4deb996718731a09b13fbc4e9a17ca268a2753f8
Serhii Kirichko <setti@setti.biz>
2019-04-01 16:09:00 +0300

    Fixed exercise 1

--- Homework/func_1.py
+++ Homework/func_1.py
@@ -7,11 +7,13 @@ def dig_check(string):
     for i in string:
         if i.isdigit():
             return False
+    return True
 
 
 while True:
-    msg = input(" Please enter the message."
-                "\n Note that entering a number in your message will stop the program: ")
-    if dig_check(msg) == False:
+    msg = input(" Please enter the message." +
+                "\n Note that entering a number in your message" +
+                " will stop the program: ")
+    if not dig_check(msg):
         break
     print(msg)
