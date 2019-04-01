96b6defb263ae96b782967ce610fef47a4e0537d
Serhii Kirichko <setti@setti.biz>
2019-04-01 16:18:28 +0300

    Fixed exercise 4

--- Homework/func_4.py
+++ Homework/func_4.py
@@ -2,12 +2,8 @@
 # и выводит *список* ключей (keys) в качестве рузельтата
 
 
-def ret_keys(arguments):
-    keys_list = []
-    for i in arguments.keys():
-        keys_list.append(i)
-    return keys_list
+def ret_keys(**arguments):
+    return arguments.keys()
 
 
-proof = ret_keys({'blue': 5, 'yellow': 7, 'red': 9, 'orange': 13})
-print(proof)
+print(ret_keys(blue=5, yellow=7, red=9, orange=13))
