4ea88a09d42114c76c51b95abcb5e9bbdbfe2bac
Serhii Kirichko <setti@setti.biz>
2019-04-01 16:13:00 +0300

    Fixed exercise 2

--- Homework/funk_2.py
+++ Homework/funk_2.py
@@ -1,11 +1,8 @@
 # Написать функцию, которая принимает любое количество аргументов и печатает последний элемент.
 
 
-def ret_last(*args):
-    checklist = []
-    for i in args:
-        checklist.append(i)
-    print(checklist[-1])
+def get_last(*args):
+    return args[-1]
 
 
-ret_last(1, 2, 3, 4, 5, 6, 18)
+get_last(1, 2, 3, 4, 5, 6, 18)
