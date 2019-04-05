6a47e802fa265e72f4bab18887b9beea7d9553f9
Ruslan <olkhovskyrus@gmail.com>
2019-04-05 17:21:14 +0300

    added docstrings

--- dice.py
+++ dice.py
@@ -2,10 +2,12 @@ from random import randint
 
 
 class Dice:
+    """Imitates the dice which can have as many sides as user wants. Default dice has 6 sides"""
     def __init__(self, sides=6):
         self.sides = sides
 
     def roll_the_dice(self):
+        """Imitates the dice roll. Prints a random number between 1 and the amount of sides"""
         print(randint(1, self.sides))
 
 
