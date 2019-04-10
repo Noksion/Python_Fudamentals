11f6d48c2e5c4fa60844e1cc17086985fbcbb5a3
Serhii Kirichko <setti@setti.biz>
2019-04-10 13:15:00 +0300

    Test module

--- test/test_example.py
+++ test/test_example.py
@@ -3,7 +3,7 @@ import unittest
 from Homework.funk_2 import get_last
 
 
-class TestStringMethods(unittest.TestCase):
+class TestExample(unittest.TestCase):
 
     def test_upper(self):
         self.assertEqual(4, get_last(1, 2, 3, 4))
