fd9933d8a44c75bf4c6f51174d337fc9c4aa8104
Ruslan <olkhovskyrus@gmail.com>
2019-04-12 18:29:55 +0300

    Tests

--- city_function.py
+++ city_function.py
@@ -1,4 +1,3 @@
 def format_geo(city, country):
     """Returns city and country names neatly formatted"""
-    result = city.title() + ", " + country.title()
-    return result
+    return city.title() + ", " + country.title()
